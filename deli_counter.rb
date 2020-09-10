8e2c34a8048fc443e527c7d12379ad64bd53dd4a
Brock Byrd <brockbyrdd@gmail.com>
2020-09-10 21:35:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,12 +7,14 @@ def line(array)
   if array.length <= 0
     "The line is currently empty."
   else
-    array.each_with_index |name, index|
+    array.each_with_index { |name, index|
       if index.length != (array.length - 1)
         phrase += "#{index + 1}. #{name}"
       else
         phrase += "#{index + 1}. #{name}"
       end
+    }
+    puts phrase
 end
 
 def take_a_number(array, name)
