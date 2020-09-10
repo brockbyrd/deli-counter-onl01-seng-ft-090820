ae0561b1a52a60a5f5125d62d9ba8c4c07916aab
Brock Byrd <brockbyrdd@gmail.com>
2020-09-10 21:31:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  phrase = "The line is currently: "
+  
+  if array.length <= 0
+    "The line is currently empty."
+  else
+    array.each_with_index |name, index|
+      if index.length
+  
+end
+
+def take_a_number(array, name)
+  
+end
+
+def now_serving(array)
+  
+end
