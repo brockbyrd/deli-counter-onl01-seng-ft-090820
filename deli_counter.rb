72677d695186b95d0dbad37127265d4727385f74
Brock Byrd <brockbyrdd@gmail.com>
2020-09-10 22:01:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,7 +24,7 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  if array <= 0
+  if array.length <= 0
     "The line is currently empty."
   else
     "Now serving #{array.shift}."
